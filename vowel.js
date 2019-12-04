c246d15511cd5d428ac43588e33c8e01e5f509e7
SeR4h <40196738+SeR4h@users.noreply.github.com>
2019-12-04 13:12:23 +0300

    final vowel modification

--- vowel.js
+++ vowel.js
@@ -3,28 +3,32 @@ function countVowels(paragraph){
     var counter = 0;
     var characterCounter={}
     
-    var set1= new Set()
+    var vowelSet= new Set()
     var vowels=['a','e','i','o','u'] 
     
     for(var i=0;i<=paragraph.length;i++){
     if(vowels.includes(paragraph.charAt(i))){
-    set1.add(paragraph.charAt(i))
+    vowelSet.add(paragraph.charAt(i))
     }
     if(!characterCounter.hasOwnProperty(paragraph.charAt(i))){
     characterCounter[paragraph.charAt(i)] = 1
     //characterCounter[i]: myValue
     //add it as key to my bject with value of 1
     }
-    else if(characterCounter[(paragraph.charAt(i))] == 1){
-     counter =counter+ 1 
-    characterCounter[paragraph.charAt(i)] =characterCounter[paragraph.charAt(i)] +1
-    
+    else{ 
+ 	if(characterCounter[(paragraph.charAt(i))] == 1){
+  
+ counter =counter+ 1
+     }
+    characterCounter[paragraph.charAt(i)] =characterCounter[paragraph.charAt(i)] +1  
     }
     }
-    
-    newVowel= Array.from(set1).join('')
+      console.log(characterCounter)
+    var newVowel= Array.from(vowelSet).join('')
     var myArray= [newVowel,counter]
     console.log(myArray)
     }
-    var s="This is a test string"
-    countVowels(s)
+    var str="This is a test string "
+    var finalstring = str.split(' ').join('')
+   
+    countVowels(finalstring)
