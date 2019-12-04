a28ea03240276cbe94b536aeede9e237bfae3fd9
SeR4h <40196738+SeR4h@users.noreply.github.com>
2019-12-04 18:41:06 +0300

    final fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,24 @@
+ 
+function fizzbuzz(Fiz=[],Buz=[]){
+    var x =  Fiz.length + Buz.length;
+    if(x%5==0&&x%3==0){ 
+    return 'Fizzbuzz'
+    }
+    if(x%3==0){
+     return 'Fizz'
+    }
+    if(x%5==0){
+     return 'Buzz'
+    }
+    if(!(x%5==0)&&!(x%3==0)){
+        return x
+       }
+    }
+    /*var a= [ 's', 'a', 'r','a','h' ]; 
+    var b= [ 'n', 'a', 'k','y', 'e', 'm', 'b','e','S','N'];
+    var c=['s', 'e', 'r','a'];    
+    console.log(fizzbuzz(a,b));
+    console.log(fizzbuzz(a,a));
+    console.log(fizzbuzz(a,c));*/
+
+module.exports = fizzbuzz;
